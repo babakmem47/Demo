edea433ef80ff7a21697301ca7a3a7f39cf3b586
babak35102 <babak35102@gmail.com>
2019-02-27 10:56:24 +0330

    Enable client side validation. Toggle Province div in Create.cshtml

--- Demo/ViewModels/SetadFormViewModel.cs
+++ Demo/ViewModels/SetadFormViewModel.cs
@@ -7,28 +7,33 @@ namespace Demo.ViewModels
     public class SetadFormViewModel
     {
         [Display(Name = "نام")]
+        [Required(ErrorMessage = "پر کردن فیلد نام اجباری است")]
         public string Name { get; set; }
 
+        [Display(Name = "رنج آی پی")]
+        [Required(ErrorMessage = "انتخاب آی پی اجباری است")]
+        public int IpRangeId { get; set; }
+
+        public IEnumerable<IpRange> IpRanges { get; set; }
+
         [Display(Name = "شاخص")]
+        [StringLength(4, ErrorMessage = "طول فیلد حداکثر چهار رقم")]
+        [Range(1, 9999, ErrorMessage = "مقدار غیر مجاز می باشد")]
         public string Shakhes { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [EmailAddress(ErrorMessage = "آدرس ایمیل صحیح نیست")]
+        public string Email { get; set; }
 
         [Display(Name = "آدرس")]
         public string Address { get; set; }
+
+        [Display(Name = "آیا مدیریت شعب است؟")]
+        public bool IsModiriatShoab { get; set; }
 
         [Display(Name = "استان")]
         public int? ProvinceId { get; set; }                    // use it in DropDownList
 
         public IEnumerable<Province> Provinces { get; set; }   // created for showing the list in DropDown
-
-        [Display(Name = "آیا مدیریت شعب است؟")]
-        public bool IsModiriatShoab { get; set; }
-
-        [Display(Name = "رنج آی پی")]
-        public int IpRangeId { get; set; }
-
-        public IEnumerable<IpRange> IpRanges { get; set; }
-
-        [Display(Name = "ایمیل")]
-        public string Email { get; set; }
     }
 }
