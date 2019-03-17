d19611ac906d0b7679bc89335d155239d1a00556
babak35102 <babak35102@gmail.com>
2019-03-17 11:21:10 +0330

    Add a link below Ip Range combobox for new IpRange + Add IpController + Create Action 'Create' + Create views for Index and Create + Test link

---
+++ Demo/Controllers/IpController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class IpController : Controller
+    {
+        // GET: Ip
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
